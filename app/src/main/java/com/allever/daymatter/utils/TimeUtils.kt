0e7cbcc866cae3de0a72230588b9dc72e159ce67
devallever <devallever@163.com>
2019-05-22 17:54:07 +0800

    内容: 添加时间工具类

---
+++ app/src/main/java/com/allever/daymatter/utils/TimeUtils.kt
@@ -0,0 +1,36 @@
+package com.allever.daymatter.utils
+
+object TimeUtils {
+
+    private var startTime = 0L
+    private var prevTime = 0L
+    fun start() {
+        startTime = System.currentTimeMillis()
+        prevTime = startTime
+    }
+
+    fun getInterval(): Long {
+        val currentTimeMillis = System.currentTimeMillis()
+        val interval = currentTimeMillis - prevTime
+        prevTime = currentTimeMillis
+        return interval
+    }
+
+
+    fun formatTime(time: Long): String {
+        var min = (time / (1000 * 60)).toString() + ""
+        var sec = (time % (1000 * 60)).toString() + ""
+        min = if (min.length < 2) {
+            "0" + time / (1000 * 60) + ""
+        } else {
+            (time / (1000 * 60)).toString() + ""
+        }
+        when {
+            sec.length == 4 -> sec = "0" + time % (1000 * 60) + ""
+            sec.length == 3 -> sec = "00" + time % (1000 * 60) + ""
+            sec.length == 2 -> sec = "000" + time % (1000 * 60) + ""
+            sec.length == 1 -> sec = "0000" + time % (1000 * 60) + ""
+        }
+        return min + ":" + sec.trim { it <= ' ' }.substring(0, 2)
+    }
+}
